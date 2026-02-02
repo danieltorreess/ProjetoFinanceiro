c80ad4f018d33674f8399e219213767f18c98d65
Daniel Torres <cicerodaniel.torres@hotmail.com>
2026-02-02 20:28:28 -0300

    Voltei atrás ao flow, ele não funcionou upando minhas tabelas automaticamente, preciso optar por banco em nuvem ou uma API diferente sem requisões...

--- src/app.py
+++ src/app.py
@@ -64,10 +64,10 @@ print("==============================================\n")
 # ==========================================
 # 🔄 Refresh do Power BI
 # ==========================================
-from powerbi.push import refresh_dataset
+# from powerbi.push import refresh_dataset
 
-WORKSPACE_ID = "3c950437-3a73-4270-b175-d5b8c5edd24f"
-DATASET_ID = "03fc1e7f-8c53-412f-87e5-f2857411d7ac"
+# WORKSPACE_ID = "3c950437-3a73-4270-b175-d5b8c5edd24f"
+# DATASET_ID = "03fc1e7f-8c53-412f-87e5-f2857411d7ac"
 
-print("🔄 Iniciando refresh do Power BI...")
-refresh_dataset(WORKSPACE_ID, DATASET_ID)
+# print("🔄 Iniciando refresh do Power BI...")
+# refresh_dataset(WORKSPACE_ID, DATASET_ID)
